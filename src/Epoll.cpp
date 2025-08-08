6e8ca860e6db5ac016a619073f431e33d9307736
xfLinux <xf@1225041397@qq.com>
2025-08-08 18:48:04 +0800

    添加Epoll函数实现，但是未具体实现，仅仅是为了编译通过

---
+++ src/Epoll.cpp
@@ -0,0 +1,25 @@
+#include "Epoll.h"
+
+Epoll::Epoll() {
+
+}
+
+Epoll::~Epoll() {
+
+}
+
+void Epoll::del_epoll(int fd) {
+
+}
+
+void Epoll::mod_epoll(int fd, uint32_t events) {
+
+}
+
+void Epoll::add_epoll(int fd, std::shared_ptr<Connection>) {
+
+}
+
+void Epoll::loop() {
+
+}
